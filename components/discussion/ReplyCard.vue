85ba29567d45fbc94e489042e6f0aea76806a261
Abhay Chandel <abhay1072@gmail.com>
2020-01-13 14:04:05 +0530

    User profile across the card

--- components/discussion/ReplyCard.vue
+++ components/discussion/ReplyCard.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card class="answer-card">
     <!-- <v-container fluid style class="py-1 pl-2 pr-4 pr-sm-6"> -->
-    <v-container fluid style class="pt-1 pb-4 pl-3 pr-5 pr-sm-6">
+    <v-container fluid style class="pt-1 pb-4 pl-5 pr-5 pr-sm-6">
       <v-row justify="end" class="paddingMarginZero reportMenuRowHeight">
         <v-menu
           bottom
@@ -40,6 +40,15 @@
           @click="showReportViolationCard = !showReportViolationCard"
         ></v-icon>
       </v-row>
+      <v-row class="pa-0 ma-0 d-flex flex-column">
+        <div class="userProfileLinkFont pa-0 ma-0">{{ username }}</div>
+        <div class="activityDetailsLabelFont pa-0">
+          Replied:&nbsp;{{ datePosted }}
+        </div>
+        <!-- <span class="userProfileLinkFont">{{ username }}</span> &nbsp;&nbsp;
+            <span class="linkSupportingWordFont">replied</span> -->
+      </v-row>
+      <v-divider class="mt-2 mb-4 mt-sm-3 mb-sm-6"></v-divider>
       <v-row justify="center" class="paddingMarginZero">
         <v-col
           id="vote-col"
@@ -51,15 +60,9 @@
           <span>{{ likes }}</span>
         </v-col>
         <v-col id="question-col" cols="10" md="11" class="pa-0">
-          <v-row class="pa-0 ma-0 d-flex flex-column">
-            <div class="userProfileLinkFont pa-0 ma-0">{{ username }}</div>
-            <div class="activityDetailsLabelFont pa-0">
-              Replied:&nbsp;{{ datePosted }}
-            </div>
-            <!-- <span class="userProfileLinkFont">{{ username }}</span> &nbsp;&nbsp;
-            <span class="linkSupportingWordFont">replied</span> -->
+          <v-row class="ma-0 replyFont">
+            {{ reply }}
           </v-row>
-          <v-row class="pt-3 ma-0 replyFont">{{ reply }}</v-row>
         </v-col>
       </v-row>
     </v-container>
