b9cd7077e2fccaca51457f8a8ba1ac87678e9fbf
Abhay Chandel <abhay1072@gmail.com>
2020-01-19 13:34:13 +0530

    Added Axios request interceptor to add Authorization header

--- components/auth/LoginCard.vue
+++ components/auth/LoginCard.vue
@@ -10,7 +10,7 @@
         </v-row>
         <v-row class="d-flex flex-row justify-center ma-0 pa-0">
           <v-alert
-            value="dialogMessage != null"
+            value="dialogMessage!=null"
             outlined
             dense
             color="pink"
