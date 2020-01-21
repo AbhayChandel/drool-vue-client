f6207bd0914e85ef02527ccc6282953d67bff53f
Abhay Chandel <abhay1072@gmail.com>
2020-01-21 17:37:03 +0530

    Implemented Snackbar for alerts globally

---
+++ components/common/error/AlertSnackbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-snackbar v-model="snackbarOpen" :timeout="0" color="error" class="red">
+    {{ snackbarMessage }}
+    <v-btn color="red" text @click="closeSnackbar">
+      Close
+    </v-btn>
+  </v-snackbar>
+</template>
+
+<script>
+import { mapState, mapMutations } from "vuex";
+
+export default {
+  data: () => ({}),
+  computed: {
+    ...mapState("common/alertsnackbar", ["snackbarOpen", "snackbarMessage"])
+  },
+  methods: {
+    ...mapMutations({
+      closeSnackbar: "common/alertsnackbar/closeSnackbar"
+    })
+  }
+};
+</script>
---
+++ store/common/alertsnackbar.js
@@ -0,0 +1,29 @@
+export const state = () => ({
+  snackbarOpen: false,
+  snackbarMessage: ""
+});
+
+export const mutations = {
+  openSnackbar(state, message) {
+    state.snackbarOpen = true;
+    state.snackbarMessage = message;
+  },
+  closeSnackbar(state) {
+    state.snackbarOpen = false;
+    state.snackbarMessage = "";
+  }
+};
+
+export const actions = {
+  openCloseSnackbar(vuexContext, message) {
+    return new Promise((resolve, reject) => {
+      vuexContext.commit("openSnackbar", message);
+      setTimeout(() => {
+        vuexContext.commit("closeSnackbar");
+        resolve();
+      }, 3000);
+    });
+  }
+};
+
+export const getters = {};
