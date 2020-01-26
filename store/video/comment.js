f6f5514ddd643f0bc4d9c20e351189c42bb8c2c7
Abhay Chandel <abhay1072@gmail.com>
2020-01-26 10:07:09 +0530

    Added video comment script

---
+++ store/video/comment.js
@@ -0,0 +1,60 @@
+export const state = () => ({});
+
+export const actions = {
+  saveComment(vuexContext, details) {
+    return new Promise((resolve, reject) => {
+      /* console.log(
+        "discussionTopicId: " +
+          details.discussionTopicId +
+          ". reply: " +
+          details.reply +
+          ". " +
+          "userId: " +
+          details.userId
+      );
+      this.$axios
+        .$post("/discussion/reply/post", {
+          discussionTopicId: details.discussionTopicId,
+          reply: details.reply,
+          userId: details.userId
+        })
+        .then(data => {
+          console.log(data);
+          resolve(data);
+        })
+        .catch(error => {
+          reject();
+        }); */
+      resolve(); // remove this after actual to call to server
+    });
+  }
+  /* toggleReplyLike(vuexContext, details) {
+    return new Promise((resolve, reject) => {
+      console.log(
+        "postId: " +
+          details.postId +
+          ". userId: " +
+          vuexContext.rootState.user.account.userDetails.userId +
+          ". " +
+          details.toggleType
+      );
+      this.$axios
+        //.$put(`discussion/reply/post/likes/${details.toggleType}`, {
+        .$put(`/discussion/reply/likes/${details.toggleType}`, {
+          postId: details.postId,
+          currentUserId: vuexContext.rootState.user.account.userDetails.userId
+        })
+        .then(data => {
+          console.log(data);
+          resolve(true);
+        })
+        .catch(error => {
+          reject(false);
+        });
+    });
+  } */
+};
+
+export const mutations = {};
+
+export const getters = {};
